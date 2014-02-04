08ea85207c0138573bea738a39090d2f355aaab7
wojtekbe <wojtekbe@gmail.com>
2014-02-04 12:57:20 +0000

    sidedriver/readme added

--- sidedriver/driver-discovery/main.c
+++ sidedriver/driver-discovery/main.c
@@ -34,13 +34,24 @@ void init_timer4(void)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; 
 	GPIOD->MODER |= GPIO_MODER_MODER13_1;
-	GPIOD->AFR[1] |= 2 << 20;
+	GPIOD->AFR[1] |= (2 << 20);
 	TIM4->PSC =   5;
 	TIM4->ARR =   100;
 	TIM4->CCR2 =  10;
 	TIM4->CCMR1 = TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; // Output Compare 2 Mode = 110 (PWM 1 Mode)
 	TIM4->CCER |= TIM_CCER_CC2E; // Capture/Compare 2 output enable
 	TIM4->CR1 =   TIM_CR1_CEN;
+}
+
+void init_i2c(void) 
+{
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+	GPIOB->MODER |= GPIO_MODER_MODER7_1 | GPIO_MODER_MODER8_1;
+	GPIOB->AFR[0] |= (4 << 28); // AF4 for PB7
+	GPIOB->AFR[1] |= (4 << 0);  // AF4 for PB8
+	I2C1->OAR1 = 0x18;
+	I2C1->CR1 |= I2C_CR1_ACK | I2C_CR1_ENGC | I2C_CR1_PE;
 }
 
 void Delay(__IO uint32_t nCount)
@@ -53,7 +64,8 @@ void Delay(__IO uint32_t nCount)
 int main(void)
 {
 	init_usart();
-	init_timer4();
+	//init_timer4();
+	init_i2c();
 	while(1){
 		USART_SendData(USART2, 'a');
 		Delay(0x3FFFFF);
--- sidedriver/driver/main.c
+++ sidedriver/driver/main.c
@@ -1,60 +1,62 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_usart.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
 
-void init_usart(void){
+void _wait(__IO uint32_t i)
+{
+  while(i--) { }
+}
 
-	GPIO_InitTypeDef GPIO_InitStructure;
+void init_usart(void)
+{	
 	USART_InitTypeDef USART_InitStructure;
 
-	/* enable peripheral clock for USART2 */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	
+	GPIOA->MODER |= GPIO_MODER_MODER9_1;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR9_0;
+	GPIOA->AFR[1] |= (7 << 4);
 
-
-	/* GPIOA clock enable */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-	/* GPIOA Configuration:  USART2 TX on PA2 */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	/* Connect USART2 pins to AF2 */
-	// TX = PA2
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	//USART2->BRR = 0x000100E5;
+	//USART2->CR1 |= USART_CR1_TE | USART_CR1_UE;
+	
+	USART_InitStructure.USART_BaudRate = 115200;
 	USART_InitStructure.USART_Mode = USART_Mode_Tx;
-	USART_Init(USART2, &USART_InitStructure);
-
-	USART_Cmd(USART2, ENABLE); // enable USART2
-
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 }
 
-void Delay(__IO uint32_t nCount)
+/*
+void usart_send_uint16(uint16_t i)
 {
-  while(nCount--)
-  {
-  }
+	//USART_SendData(USART2, (uint8_t)((i >> 24) & 0x000000FF));
+	//USART_SendData(USART2, (uint8_t)((i >> 16) & 0x000000FF));
+	USART_SendData(USART2, (uint8_t)((i >> 8) & 0x00FF));
+	USART_SendData(USART2, (uint8_t)(i & 0x00FF));
+}
+*/
+
+void init_timer2(void) // PA1 -> M1-PWM
+{
+	//RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER1_1;
+	GPIOA->AFR[0] |= (1 << 4);
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; 
+	TIM2->PSC =   5;
+	TIM2->ARR =   100;
+	TIM2->CCR2 =  10;
+	TIM2->CCMR1 = TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; // Output Compare 2 Mode = 110 (PWM 1 Mode)
+	TIM2->CCER = TIM_CCER_CC2E; // Capture/Compare 2 output enable
+	TIM2->CR1 |= TIM_CR1_CEN; // Counter Enable
 }
 
-int main(void){
 
+int main(void)
+{
 	init_usart();
-
-	//uart_puts("Init complete!");
-
+	init_timer2();
 	while(1){
-		USART_SendData(USART2, 'h'); // defined in stm32f4xx_usart.h
-		Delay(0x3FFFFF);
+		USART_SendData(USART1, 'a');
+		_wait(0xFFFFF);
 	}
-
 }
