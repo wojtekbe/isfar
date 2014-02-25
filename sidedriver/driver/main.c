41c1a107409b48d5aa14f4d4e44a0c0cb7161d56
Wojciech Bieganski <wbieganski@antmicro.com>
2014-02-25 16:00:26 +0000

    25-02

--- sidedriver/driver/main.c
+++ sidedriver/driver/main.c
@@ -5,14 +5,14 @@
 #include <unistd.h>
 
 int _write(int, char*, int);
-void init_leds(void);
-void init_usart(void);
-void usart_send(uint8_t);
-void init_timer4(void);
-void init_timer3(void);
-void init_i2c(void);
-void wait(uint32_t);
 
+void _wait(uint32_t nCount)
+{
+  while(nCount--)
+  {
+	  __asm__("NOP");
+  }
+}
 
 int _write(int file, char *ptr, int len)
 {
@@ -27,38 +27,19 @@ int _write(int file, char *ptr, int len)
 		}
 		return i;
 	}
-	//errno = EIO;
 	return -1;
-}
-
-void usart_send(uint8_t d)
-{
-	while ((USART1->SR & USART_SR_TXE) == 0);
-	USART1->DR = d;
-}
-
-void init_leds(void)
-{
-	// PD11-15 as outputs
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-	GPIOD->MODER |= GPIO_MODER_MODER11_0;
-	GPIOD->MODER |= GPIO_MODER_MODER12_0;
-	GPIOD->MODER |= GPIO_MODER_MODER13_0;
-	GPIOD->MODER |= GPIO_MODER_MODER14_0;
-	GPIOD->MODER |= GPIO_MODER_MODER15_0;
 }
 
 void init_usart(void)
 {	
 	USART_InitTypeDef USART_InitStructure;
 
-	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 	
-	GPIOA->MODER |= GPIO_MODER_MODER2_1;
-	//GPIOA->PUPDR |= GPIO_PUPDR_PUPDR2_0;
-	GPIOA->AFR[0] |= 0x0000700;
-
+	GPIOA->MODER |= GPIO_MODER_MODER9_1;
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR9_0;
+	GPIOA->AFR[1] |= (7 << 4);
 
 	USART_InitStructure.USART_BaudRate = 115200;	
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -70,113 +51,56 @@ void init_usart(void)
 	USART_Cmd(USART1, ENABLE); // enable USART1
 }
 
-void init_timer4(void)
+void usart_send(uint8_t d)
 {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; 
-	GPIOD->MODER |= GPIO_MODER_MODER13_1;
-	GPIOD->AFR[1] |= (2 << 20);
-	TIM4->PSC =   16799;
-	TIM4->ARR =   99;
-	TIM4->CCR2 =  50;
-	TIM4->CCMR1 = TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; // Output Compare 2 Mode = 110 (PWM 1 Mode)
-	TIM4->CCER |= TIM_CCER_CC2E; // Capture/Compare 2 output enable
-	TIM4->CR1 |= 0 | TIM_CR1_CEN;
+	while ((USART1->SR & USART_SR_TXE) == 0);
+	USART1->DR = d;
 }
 
-void init_timer3()
+void init_timer2(void) // PA1 -> M1-PWM
 {
-	// Capturing on PC6
+	//RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER1_1;
+	GPIOA->AFR[0] |= (1 << 4);
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; 
+	TIM2->PSC =   5;
+	TIM2->ARR =   100;
+	TIM2->CCR2 =  10;
+	TIM2->CCMR1 = TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; // Output Compare 2 Mode = 110 (PWM 1 Mode)
+	TIM2->CCER = TIM_CCER_CC2E; // Capture/Compare 2 output enable
+	TIM2->CR1 |= TIM_CR1_CEN; // Counter Enable
+}
+
+void init_timer3(void) // PC9 -> LED-PWM (TIM3_CH4)
+{
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-	GPIOC->MODER |= GPIO_MODER_MODER6_1; // AF for PC6
-	GPIOC->AFR[0] |= (2 << 24); // AF2 for PC6
-	TIM3->PSC = 10000-1;
-	TIM3->ARR = 1000-1;
-	TIM3->CCMR1 |= TIM_CCMR1_CC1S_0; // CC1 channel is conf. as INPUT, IC1 is mapped on TI1
-	TIM3->CCER |= TIM_CCER_CC1E; 
-	TIM3->CR1 |= TIM_CR1_CEN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; 
+	GPIOC->MODER |= GPIO_MODER_MODER9_1;
+	GPIOC->AFR[1] |= (2 << 4); // AF2
+	TIM2->PSC =   5;
+	TIM2->ARR =   100;
+	TIM2->CCR4 =  10;
+	TIM2->CCMR2 = TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1; // Output Compare 4 Mode = 110 (PWM 1 Mode)
+	TIM2->CCER = TIM_CCER_CC4E; // Capture/Compare 4 output enable
+	TIM2->CR1 |= TIM_CR1_CEN; // Counter Enable
 }
 
-void init_i2c(void) 
+void init_led(void)
 {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
-	GPIOB->OTYPER |= (1 << 7) | (1 << 8); // Open drain PB7,8
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR7_0 | GPIO_PUPDR_PUPDR8_0; // Enable Pull-Ups for PB7,8
-	GPIOB->MODER |= GPIO_MODER_MODER7_1 | GPIO_MODER_MODER8_1;
-	GPIOB->AFR[0] |= (4 << 28); // AF4 for PB7
-	GPIOB->AFR[1] |= (4 << 0);  // AF4 for PB8
-	I2C1->OAR1 = (0x09 << 1) | (1 << 14);
-	NVIC_EnableIRQ(I2C1_EV_IRQn); // Enable I2C1 EV IRQ
-	NVIC_EnableIRQ(I2C1_ER_IRQn); // Enable I2C1 ER IRQ
-	I2C1->CR2 |= I2C_CR2_ITERREN | I2C_CR2_ITEVTEN | I2C_CR2_ITBUFEN; 
-	I2C1->CR1 |= I2C_CR1_ACK | I2C_CR1_PE;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	GPIOC->MODER |= GPIO_MODER_MODER9_0;
+	GPIOC->ODR |= (1 << 9);
 }
 
-
-void I2C1_EV_IRQHandler() 
-{
-	int stat1, stat2, data;
-
-	if(I2C1->SR1 & I2C_SR1_ADDR) // ADDR matched
-	{
-		stat1 = I2C1->SR1;
-		stat2 = I2C1->SR2;
-	}
-/*	else if(I2C1->SR1 & I2C_SR1_RXNE) // Recv. reg. not empty
-	{		
-		LEDT(GREEN);
-	}
-*/	else if(I2C1->SR1 & I2C_SR1_TXE) // Transm. reg. empty
-	{	
-		//LEDT(BLUE);
-		I2C1->DR = 10;
-	}	
-/*	else if(I2C1->SR1 & I2C_SR1_BTF) // Byte Transfer Finished
-	{		
-		//LEDT(ORANGE);
-	}
-*/	else if(I2C1->SR1 & I2C_SR1_STOPF) // STOP received
-	{
-		stat1 = I2C1->SR1;
-		I2C1->CR1 |= I2C_CR1_STOP;
-	}
-	else if(I2C1->SR1 != 0)
-		printf("EV_IRQ_Handler: SR1= 0x%08x SR2 = 0x%08x DR=0x%08x\n ", I2C1->SR1, I2C1->SR2, I2C1->DR);
-}
-
-void I2C1_ER_IRQHandler() 
-{
-	int stat1, stat2;
-	//LEDT(RED);
-	if(I2C1->SR1 & I2C_SR1_AF) // ACK failure (NACK)
-		I2C1->SR1 &= ~I2C_SR1_AF;
-	else
-		printf("ER_IRQ_Handler: SR1= 0x%04x SR2 = 0x%04x DR=0x%04x\n ", I2C1->SR1, I2C1->SR2, I2C1->DR);
-}
-
-void wait(uint32_t nCount)
-{
-  while(nCount--)
-  {
-	  __asm__("NOP");
-  }
-}
 
 int main(void)
 {
-	//init_leds();
 	init_usart();
-	//init_timer3();
-	//init_i2c();
-	printf("\nHello\n");
+	init_led();
+	//init_timer2();
+	//printf("Hello\n");
 	while(1){
-		//LED(12, 1);
-		//wait(600000);
-		//printf("CCR1=0x%08x\n", TIM3->CCR1);
-		//LED(12, 0);
-		//printf("SR1=0x%04x, SR2=0x%04x\n", I2C1->SR1, I2C1->SR2);
-		wait(6000000);
+		usart_send('a');
+		_wait(0xFFFFF);
 	}
 }
