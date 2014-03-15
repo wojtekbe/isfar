0636f365d882838154690a2e1a346cb9fe71f0a7
Wojciech Bieganski <wbieganski@antmicro.com>
2014-03-15 15:18:31 +0000

    siderdriver: added i2c support

--- sidedriver/driver/main.c
+++ sidedriver/driver/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// prototypes
 void _wait(uint32_t);
 void init_usart(void);
 int _write(int, char*, int);
@@ -17,13 +18,18 @@ void tankd_disable(void);
 void tankd_dir(int);
 void tankd_sp(int);
 
-struct tankd_state
-{
-	int init;
-	int enabled;
-	int dir;
-	int piston_pos;	
-};
+
+#define I2C_ADDR 0x09
+
+#define STATE 0x00
+#define TANKD_ENABLED 0x01
+#define MOTORD_ENABLED 0x02
+#define LEDD_ENABLED 0x03
+
+// globals
+uint16_t i2c_regs[4] = {0, 0, 0, 0};
+int i2c_bytes_received;
+int i2c_reg_idx;
 
 void _wait(uint32_t nCount)
 {
@@ -78,6 +84,94 @@ void usart_send(uint8_t d)
 {
 	while ((USART1->SR & USART_SR_TXE) == 0);
 	USART1->DR = d;
+}
+
+void init_i2c(void) 
+{
+	/*
+	 * SDA	PB9		I2C1_SDA
+	 * SCL	PB8		I2C1_SCL
+	 */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+	GPIOB->OTYPER |= (1 << 8) | (1 << 9); // Open drain PB7,8
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR9_0; // Enable Pull-Ups for PB7,8
+	GPIOB->MODER |= GPIO_MODER_MODER8_1 | GPIO_MODER_MODER9_1; // AF
+	GPIOB->AFR[1] |= (4 << 4); // AF4 for PB9 SDA
+	GPIOB->AFR[1] |= (4 << 0); // AF4 for PB8 SCL
+	I2C1->OAR1 = (I2C_ADDR << 1) | (1 << 14);
+	NVIC_EnableIRQ(I2C1_EV_IRQn);
+	NVIC_EnableIRQ(I2C1_ER_IRQn);
+	I2C1->CR2 |= I2C_CR2_ITERREN | I2C_CR2_ITEVTEN | I2C_CR2_ITBUFEN; 
+	I2C1->CR1 |= I2C_CR1_ACK | I2C_CR1_PE;
+
+	i2c_reg_idx = 0;
+	i2c_bytes_received = 0;
+}
+
+
+void I2C1_EV_IRQHandler() 
+{
+	uint16_t temp;
+	uint16_t stat1 = 0;
+	//uint16_t stat2 = 0;
+	uint32_t data;
+
+	stat1 = I2C1->SR1;
+	//stat2 = I2C1->SR2;
+
+	if(stat1 & I2C_SR1_ADDR) // ADDR matched, EV1
+	{
+		temp = I2C1->SR1;
+		temp = I2C1->SR2;
+		i2c_bytes_received = 0;
+	}
+	
+	if(stat1 & I2C_SR1_RXNE) // Recv. reg. not empty EV2
+	{		
+		data = I2C1->DR;
+		i2c_bytes_received++;
+		if(i2c_bytes_received == 1)
+			i2c_reg_idx = data;
+		else
+			i2c_regs[i2c_reg_idx] = data;
+	}
+	
+	if(stat1 & I2C_SR1_TXE) // Transm. reg. empty
+	{	
+		I2C1->DR = i2c_regs[i2c_reg_idx];
+	}	
+	
+	if(stat1 & I2C_SR1_BTF) // Byte Transfer Finished
+	{
+		// nothing? really?
+	}
+	
+	if(stat1 & I2C_SR1_STOPF) // STOP received, EV4
+	{
+		// clear this flag
+		temp = I2C1->SR1;
+		I2C1->CR1 |= I2C_CR1_PE;
+	}
+}
+
+void I2C1_ER_IRQHandler() 
+{
+	uint16_t stat1 = 0;
+	//uint16_t stat2 = 0;
+
+	stat1 = I2C1->SR1;
+	//stat2 = I2C1->SR2;
+
+	if(stat1 & I2C_SR1_AF) // ACK failure (NACK)
+	{
+		I2C1->SR1 &= ~I2C_SR1_AF;
+	}
+	
+	if(stat1 & I2C_SR1_OVR) // Overflow
+	{
+		I2C1->SR1 &= ~I2C_SR1_OVR;
+	}
 }
 
 void init_motord(void)
@@ -233,7 +327,7 @@ int main(void)
 	//init_motord();
 	tankd_init();
 	tankd_enable();
-	tankd_sp(10);
+	tankd_sp(15);
 	tankd_dir(-1);
 	//init_ledd();
 	printf("Hello\n");
