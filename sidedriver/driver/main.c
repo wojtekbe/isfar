ffd01b6defa1704f046f63e84a006ceb28ade815
Wojciech Bieganski <wbieganski@antmicro.com>
2014-03-18 16:44:44 +0000

    sidedriver: added support for counting encoder ticks, minor improvements

--- sidedriver/driver/main.c
+++ sidedriver/driver/main.c
@@ -4,13 +4,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define DEBUG
+
+#ifdef DEBUG
+#define debug(...) printf(__VA_ARGS__);
+#else
+#define debug(...) do { } while(0)
+#endif
+
 // prototypes
 void _wait(uint32_t);
 void init_usart(void);
 int _write(int, char*, int);
 void usart_send(uint8_t);
-void init_motord(void);
 void init_ledd(void);
+
+
+void motord_init(void);
+void motord_enable(void);
+void motord_disable(void);
+void motord_dir(uint8_t);
+void motord_pwm(uint32_t);
 
 void tankd_init(void);
 void tankd_enable(void);
@@ -18,25 +32,22 @@ void tankd_disable(void);
 void tankd_dir(int);
 void tankd_sp(int);
 
-
 #define I2C_ADDR 0x09
+#define NUM_REGS 4
 
 #define STATE 0x00
-#define TANKD_ENABLED 0x01
-#define MOTORD_ENABLED 0x02
-#define LEDD_ENABLED 0x03
+#define M1_DIR 0x01
+#define M1_PWM 0x02
+#define M2_PWM 0x03
 
-// globals
-uint16_t i2c_regs[4] = {0, 0, 0, 0};
+int16_t i2c_regs[NUM_REGS];
 int i2c_bytes_received;
 int i2c_reg_idx;
 
 void _wait(uint32_t nCount)
 {
   while(nCount--)
-  {
 	  __asm__("NOP");
-  }
 }
 
 int _write(int file, char *ptr, int len)
@@ -86,7 +97,7 @@ void usart_send(uint8_t d)
 	USART1->DR = d;
 }
 
-void init_i2c(void) 
+void i2c_init(void) 
 {
 	/*
 	 * SDA	PB9		I2C1_SDA
@@ -174,45 +185,97 @@ void I2C1_ER_IRQHandler()
 	}
 }
 
-void init_motord(void)
+void motord_init(void)
 {
 	/*
 	 * M1-PWM 	PA1 	TIM2_CH2
 	 * M1-CS 	PA2 	ADC123_IN2
-	 * M1-ENA 	PC3 	out
 	 * M1-ENB 	PA3 	out
-	 * M1-INA 	PC3 	out
 	 * M1-INB 	PA4 	out
+	 * M1-INA 	PC2 	out
+	 * M1-ENA 	PC3 	out
 	 * ENC1 	PC6 	TIM8_CH1
 	 * ENC2 	PC7 	TIM8_CH2
 	 */
-	
-	// PWM: TIM2_CH2 (PA1/AF1)
+
+	debug("motord_init()\n");
+
+	// IOs	
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER1_1;
-	GPIOA->AFR[0] |= (1 << 4);
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; 
+	GPIOA->MODER |= GPIO_MODER_MODER3_0; // M1-ENB
+	GPIOA->MODER |= GPIO_MODER_MODER4_0; // M1-INB
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	GPIOC->MODER |= GPIO_MODER_MODER2_0; // M1-INA
+	GPIOC->MODER |= GPIO_MODER_MODER3_0; // M1-ENA
+
+	// PWM: TIM2_CH2 (PA1/AF1)
+	GPIOA->MODER |= GPIO_MODER_MODER1_1; // AF
+	GPIOA->AFR[0] |= (1 << 4); // AF1
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	TIM2->PSC =   83;
 	TIM2->ARR =   99;
-	TIM2->CCR2 =  10;
 	TIM2->CCMR1 = TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1;
 	TIM2->CCER = TIM_CCER_CC2E;
-	TIM2->CR1 |= TIM_CR1_CEN;
 
-	/*
 	// Encoder: TIM8_CH1/2 (PC6/7 AF3)
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 	GPIOC->MODER |= GPIO_MODER_MODER6_1; // PC6 -> AF
 	GPIOC->AFR[0] |= (3 << 24); // PC6 -> AF3
 	GPIOC->MODER |= GPIO_MODER_MODER7_1; // PC7 -> AF
 	GPIOC->AFR[0] |= (3 << 28); // PC7 -> AF3
-	TIM8->PSC = 0;
+	RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
+	TIM8->PSC = 2 - 1; // (Prescaler - 1)
 	TIM8->ARR = 0xFFFF;
 	TIM8->CCMR1 |= TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_0; // CC1S = 01, CC2S = 02 (inputs)
-	TIM8->SMCR |= TIM_SMCR_SMS_1 | TIM_SMCR_SMS_0; // Slave Mode, SMS = 011, Encoder Mode 3 p. 615
 	TIM8->CCER |= TIM_CCER_CC1P | TIM_CCER_CC2P; // CC1P = 1, CC2P = 1
+	TIM8->SMCR |= TIM_SMCR_SMS_1 | TIM_SMCR_SMS_0; // Slave Mode, SMS = 011, Encoder Mode 3 p. 615
 	TIM8->CR1 |= TIM_CR1_CEN;
-	*/
+
+	// TODO Current measure: M1-CS
+}
+
+void motord_dir(uint8_t dir)
+{
+	debug("motord_dir(%d)\n", dir);
+
+	if(dir < 0x80)
+	{
+		GPIOC->ODR |=  (1 << 2); // M1-INA = 1
+		GPIOA->ODR &= ~(1 << 4); // M1-INB = 0
+	}
+	else
+	{
+		GPIOC->ODR &= ~(1 << 2); // M1-INA = 0
+		GPIOA->ODR |=  (1 << 4); // M1-INB = 1
+	}
+}
+
+void motord_pwm(uint32_t PWM)
+{
+	debug("motord_pwm(%d)\n", PWM);
+
+	if(PWM == 0)
+		TIM2->CR1 &= ~TIM_CR1_CEN;
+	else
+	{
+		TIM2->CR1 |= TIM_CR1_CEN;
+		TIM2->CCR2 = PWM;
+	}
+}
+
+void motord_enable()
+{
+	debug("motord_enable()\n");
+
+	GPIOC->ODR |= (1 << 3); // M1-ENA = 1
+	GPIOA->ODR |= (1 << 3); // M1-ENB = 1
+}
+
+void motord_disable()
+{
+	debug("motord_disable()\n");
+
+	GPIOC->ODR &= ~(1 << 3); // M1-ENA = 0
+	GPIOA->ODR &= ~(1 << 3); // M1-ENB = 0
 }
 
 void tankd_init()
@@ -228,13 +291,19 @@ void tankd_init()
 	 * TRANS 	PC8 	? TODO
 	 */
 
+	debug("tankd_init()\n");
 	// IOs
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 	GPIOB->MODER |= GPIO_MODER_MODER1_0; // M2-ENA
 	GPIOB->MODER |= GPIO_MODER_MODER2_0; // M2-INA
-	GPIOB->MODER |= GPIO_MODER_MODER10_0; // MIN
-	GPIOB->MODER |= GPIO_MODER_MODER11_0; // MAX
-	
+
+	// TODO MIN MAX
+	SYSCFG->EXTICR[2] |= (1 << 8);  // PB10
+	SYSCFG->EXTICR[2] |= (1 << 12); // PB11
+	EXTI->IMR |= (1 << 10) | (1 << 11); // Interrupt Mask Reg.
+	EXTI->FTSR |= (1 << 10) | (1 << 11); // Falling Trigger
+	NVIC_EnableIRQ(EXTI15_10_IRQn);
+
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; 
 	GPIOA->MODER |= GPIO_MODER_MODER6_0; // M2-ENB
 	GPIOA->MODER |= GPIO_MODER_MODER5_0; // M2-INB
@@ -246,24 +315,27 @@ void tankd_init()
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; 
 	TIM3->PSC =   9;
 	TIM3->ARR =   99;
-	//TIM3->CCR3 =  25;
 	TIM3->CCMR2 = TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1; // PWM mode
 	TIM3->CCER = TIM_CCER_CC3E;
-	//TIM3->CR1 |= TIM_CR1_CEN;
 	
 	// TRANS ? 
 	//RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 	//GPIOC->MODER |= GPIO_MODER_MODER8_1;
 }
 
+void EXTI15_10_IRQHandler()
+{
+	printf("EXTI\n");
+}
+
 void tankd_sp(int sp)
 {
-	TIM3->CCR3 = sp;
-	
 	if(sp == 0)
 		TIM3->CR1 &= ~TIM_CR1_CEN;
 	else
 		TIM3->CR1 |= TIM_CR1_CEN;		
+
+	TIM3->CCR3 = sp;
 }
 
 void tankd_enable()
@@ -280,7 +352,7 @@ void tankd_disable()
 
 void tankd_dir(int d)
 {
-	printf("td: dir=%d\n", d);
+	debug("td: dir=%d\n", d);
 	
 	if(d == 0) // stop
 	{
@@ -298,7 +370,6 @@ void tankd_dir(int d)
 		GPIOA->ODR |=  (1 << 5); // M2-INB = 1
 	}
 }
-
 
 void init_ledd(void)
 {
@@ -321,16 +392,35 @@ void init_ledd(void)
 	TIM3->CR1 |= TIM_CR1_CEN; // Counter Enable
 }
 
+int update_conf()
+{
+	int r = 0;
+	while(r<NUM_REGS)
+	{
+		// chech if {motor, tank, led}d enabled!!
+		if( r == M1_DIR )
+			motord_dir(i2c_regs[M1_DIR]);
+		else if( r == M1_PWM)
+			motord_pwm(i2c_regs[M1_PWM]);
+		r++;
+	}
+}
+
 int main(void)
 {	
 	init_usart();
-	//init_motord();
+	i2c_init();
+	//motord_init();
+	//motord_enable();
 	tankd_init();
-	tankd_enable();
-	tankd_sp(15);
-	tankd_dir(-1);
+	//tankd_enable();
 	//init_ledd();
 	printf("Hello\n");
-	while(1){
+	while(1)
+	{
+		//printf("TIM8_CNT = 0x%x\n", TIM8->CNT);
+		//printf("PB10 = %x, PB11 = 0x%x\n", GPIOB->IDR & (1 << 10), GPIOB->IDR & (1 << 11));
+		update_conf();
+		_wait(60000);
 	}
 }
