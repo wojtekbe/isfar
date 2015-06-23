3ec50e38c63ec7c6ff28ce8e16e79de45bf14be9
Wojciech Bieganski <wojtekbe@gmail.com>
2015-06-23 15:14:03 +0200

    sidedriver: toggling debug LED with LEDT()

--- sidedriver/driver/debug.h
+++ sidedriver/driver/debug.h
@@ -5,6 +5,7 @@
 
 #define DEBUG
 #define LED(x) ((x) ? (GPIOA->ODR &= ~(1 << 12)) : (GPIOA->ODR |= (1 << 12)))
+#define LEDT(x) (((GPIOA->ODR & (1 << 12))) ? LED(1) : LED(0));
 
 #ifdef DEBUG
 #define debug(...) printf(__VA_ARGS__);
